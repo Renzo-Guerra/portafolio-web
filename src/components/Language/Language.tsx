a51d6fc62e740410fb78a69bb8f9e04561f26ec3
Renzo Guerra <renzoguerralatt@gmail.com>
2026-05-22 19:20:09 -0300

    FIX: Nuevamente imgs not working in prod

--- src/components/Language/Language.tsx
+++ src/components/Language/Language.tsx
@@ -9,7 +9,7 @@ export const Language = ({ fileName, name }: Props) => {
   return (
     <>
       <div className="language-container">
-        <img src={`/public/assets/icons/${fileName}`} alt={`${name} icon`} />
+        <img src={`/assets/icons/${fileName}`} alt={`${name} icon`} />
         <span>{name}</span>
       </div>
     </>
