3b9f6c28bde32d328d0cba3c33c78233b4b1c3df
Renzo Guerra <renzoguerralatt@gmail.com>
2026-05-22 11:22:47 -0300

    UPDATE: Hero information and styles

--- src/App.tsx
+++ src/App.tsx
@@ -58,8 +58,10 @@ function App() {
       <div className='hero'>
         <h1>Renzo Guerra</h1>
         <h3>Desarrollador back-end</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At animi tempore provident fuga, quae cum ratione accusamus suscipit laborum earum optio molestiae dolor illo sed vel quam. Eius, voluptatem maiores?</p>
-      </div>
+        <p>Hola, soy desarrollador back-end de Argentina enfocado en el desarrollo de APIs en spring-boot, familiarizado con los modulos spring-data y spring-security. También manejo las habilidades necesarias para generar la interfaz de usuario que se comunicará con la API. En mis flujos de trabajo integro herramientas clave como Docker para la contenedorización, Git para el control de versiones y Notion para la gestión de proyectos.</p>
+        <p>Me recibí de la tecnicatura TUDAI (Tecnicatura Universitaria en Desarrollo de Aplicaciones Informáticas) de Tandil y actualmente sigo expandiendo y adquiriendo nuevos conocimientos.</p>
+        <p>Capaz de autogestionar mi tiempo de manera efectiva durante mis proyectos independientes, así como colaborar formando parte de un equipo de trabajo.</p>
+      </div >
       <h3>Front-end</h3>
       <div className='technologies-container'>
         {frontIcons.map(icon => (
