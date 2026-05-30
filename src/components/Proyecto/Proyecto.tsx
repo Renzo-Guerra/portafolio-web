b53e4f8314057a2ae12818bc345386e5a804fbaa
Renzo Guerra <renzoguerralatt@gmail.com>
2026-05-30 18:11:51 -0300

    FIX: Proyecto image display depending on device
    Siempre se mostraba la imagen de desktop, ahora si
    se está visitando el sitio en un telefono se
    muestra la preview del proyecto en dicha
    resolucion

--- src/components/Proyecto/Proyecto.tsx
+++ src/components/Proyecto/Proyecto.tsx
@@ -10,29 +10,28 @@ export const Proyecto = ({ proyecto }: Props) => {
   return (
     <>
       <div className="card-proyecto">
-        <figure>
-          <a href={proyecto.link} target="_blank" rel="noopener noreferrer" title="Visitar sitio">
+        <a href={proyecto.link} target="_blank" rel="noopener noreferrer" title="Visitar sitio">
+          <picture>
+            <source
+              media="(min-width: 480px)"
+              srcSet={proyecto.imgSrcDesktop} />
             <img
               className="card-proyecto__preview"
-              src={proyecto.imgSrcDesktop}
-              alt={proyecto.imgAlt}
-              // Se avisa que la primer imagen es mas chica que la segunda
-              srcSet={`${proyecto.imgSrcPhone} 300w, ${proyecto.imgSrcDesktop} 800w`}
-              // Se especifican las reglas de cuando mostrar cada foto
-              sizes={`(min-width: 500px) 800px, 300px`} />
-          </a>
-          <a href={proyecto.link} target="_blank" rel="noopener noreferrer" title="Visitar sitio">
-            <h3>{proyecto.titulo}</h3>
-          </a>
-          <figcaption>{proyecto.descripcion}</figcaption>
-        </figure>
+              src={proyecto.imgSrcPhone}
+              alt={proyecto.imgAlt} />
+          </picture>
+        </a>
+        <a href={proyecto.link} target="_blank" rel="noopener noreferrer" title="Visitar sitio">
+          <h3>{proyecto.titulo}</h3>
+        </a>
+        <p>{proyecto.descripcion}</p>
         <p><strong>Stack utilizado</strong></p>
         <div className="technologies-images-container">
           {proyecto && proyecto.stack.map(stack => (
             <Language key={stack.fileName} fileName={stack.fileName} name={stack.name} />
           ))}
         </div>
-      </div>
+      </div >
     </>
   )
 }
